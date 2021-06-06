fe6895efba2e6554befe3688b7a8175aaa01ab21
sandeep1507 <76393302+sandeep1507@users.noreply.github.com>
2021-06-06 13:31:46 +0530

    Update 01 - Say 'Hello, World!' With Python.py

--- Python/01 - Introduction/01 - Say 'Hello, World!' With Python.py
+++ Python/01 - Introduction/01 - Say 'Hello, World!' With Python.py
@@ -1,1 +1,15 @@
 
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+# Difficulty: Easy
+# Max Score: 5
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+print("Hello, World!")
