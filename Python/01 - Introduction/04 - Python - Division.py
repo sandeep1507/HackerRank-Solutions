2a67c1a954e1cb8bfd9e4d2e004b65e0ac318b74
sandeep1507 <76393302+sandeep1507@users.noreply.github.com>
2021-06-06 13:35:37 +0530

    Create 04 - Python - Division.py

---
+++ Python/01 - Introduction/04 - Python - Division.py
@@ -0,0 +1,18 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/python-division/problem
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+A = int(input())
+B = int(input())
+
+print(A // B)
+print(A / B)
